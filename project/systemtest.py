8aa73e5d2a24709bf42cceae06bbffa908c7a167
Chatchai Boonman <gamehiphop9@hotmail.com>
2023-05-31 00:34:29 +0200

    update systemtest.py

--- project/systemtest.py
+++ project/systemtest.py
@@ -5,6 +5,6 @@ from unittest import TestCase
 class PipelineTest(TestCase):
     def test_execution(self):
         os.chdir('./data')
-        subprocess.run(['python', './pipeline.py'])
+        subprocess.run(['python3', './pipeline.py'])
         assert os.path.exists('./data.sqlite')
         

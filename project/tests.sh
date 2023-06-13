e07683b9f08408aeea198f04c1c477ad47c2205b
Chatchai Boonman <gamehiphop9@hotmail.com>
2023-06-13 21:14:05 +0200

    echo in test run

--- project/tests.sh
+++ project/tests.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+echo "Installing Requirements..."
 python3 -m pip install requirements.txt
 
+echo "Running Unit Tests..."
 python3 unittest.py
+
+echo "Running System Test..."
 python3 systemtest.py

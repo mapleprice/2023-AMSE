b17b5dc04622df3ba029a89817ef8fa22858ce2a
Chatchai Boonman <gamehiphop9@hotmail.com>
2023-06-13 21:23:03 +0200

    update path again

--- project/tests.sh
+++ project/tests.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+cd main/project/
 
 echo "Installing Requirements..."
 python3 -m pip install -r requirements.txt
 
 echo "Running Unit Tests..."
-python3 main/project/unittest.py
+python3 unittest.py
 
 echo "Running System Test..."
-python3 main/project/systemtest.py
+python3 systemtest.py

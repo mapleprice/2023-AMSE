65904976c3de3b08ebe666b684327417dd6e5da1
Chatchai Boonman <gamehiphop9@hotmail.com>
2023-06-13 21:31:16 +0200

    uupddate requirement

--- project/tests.sh
+++ project/tests.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 cd main/project/
 
+echo "===================================="
 echo "Installing Requirements..."
+echo "===================================="
+
 python3 -m pip install -r requirements.txt
 
+echo "===================================="
 echo "Running Unit Tests..."
+echo "===================================="
+
 python3 unittest.py
 
+echo "===================================="
 echo "Running System Test..."
+echo "===================================="
+
 python3 systemtest.py
